9e5f9075f889d00fd17e53a14f5f8b477ec130a2
Hanmoi Choi <dchoi@Hanmois-MacBook-Pro-2.local>
2014-06-26 14:27:00 +1000

    add lint feature

--- jakefile.js
+++ jakefile.js
@@ -1,4 +1,9 @@
-desc("exmaple");
-task("example", function(){
-  console.log("Hello Jake!");
+desc("default");
+task("default", ["lint"]);
+
+desc("Lint everything");
+task("lint", [], function(){
+  console.log("Lint Here!");
+  var lint = require("./build/lint/lint_runner");
+  lint.validateFile("jakefile.js", {}, {});
 });
