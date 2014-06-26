773240bc5fd0b0e3af08f0b6efe5ddf500dc0fca
Hanmoi Choi <dchoi@Hanmois-MacBook-Pro-2.local>
2014-06-26 15:46:46 +1000

    setup integration building without setup dedicated server

--- jakefile.js
+++ jakefile.js
@@ -6,7 +6,6 @@
   task("default", ["lint"]);
 
   desc("Lint everything");
-
   task("lint", [], function () {
     var lint = require("./build/lint/lint_runner");
 
@@ -16,8 +15,24 @@
 
     var options = lintOptions();
 
-    lint.validateFileList(files.toArray(), options, {});
+    var result = lint.validateFileList(files.toArray(), options, {});
+    if(!result) jake.fail("Lint Failed");
 
+  });
+
+  desc("Integration");
+  task("Integration", ["default"], function(){
+    console.log("Integration logic here");
+
+    console.log("1. Make sure 'git status' is clean");
+    console.log("2. Build on Integration box");
+    console.log("   a. Walk over to integration box");
+    console.log("   b. 'git pull'");
+    console.log("   c. 'jake'");
+    console.log("   d. If jake fails, start over");
+    console.log("3. 'git checkout integrate'");
+    console.log("4. 'git merge master --no-ff --log'");
+    console.log("5. 'git checkout master'");
   });
 
   function lintOptions() {
