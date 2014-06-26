eaa5f12b813c7a346e065f8fd758ba874f826657
Hanmoi Choi <dchoi@Hanmois-MacBook-Pro-2.local>
2014-06-26 14:52:34 +1000

    add options for JSHint

--- jakefile.js
+++ jakefile.js
@@ -13,8 +13,31 @@ task("lint", [], function(){
   files.include("**/*.js");
   files.exclude("node_modules");
 
-  var options = {node: true};
-  var globals = {};
+  var options = {
+    bitwise: true,
+    curly: false,
+    eqeqeq: true,
+    forin: true,
+    immed: true,
+    latedef: true,
+    newcap: true,
+    noarg: true,
+    noempty: true,
+    nonew: true,
+    regexp: true,
+    undef: true,
+    strict: true,
+    trailing: true,
+    node: true
+  };
+
+  var globals = {
+    describe: false,
+    it: false,
+    beforeEach: false,
+    afterEach: false
+  };
+
   lint.validateFileList(files.toArray(), options, globals);
 
 });
