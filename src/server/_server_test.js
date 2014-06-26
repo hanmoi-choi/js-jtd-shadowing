e38a2643ce170da513ed562a5bbbfeaaf25d9cad
Hanmoi Choi <dchoi@192-168-1-5.tpgi.com.au>
2014-06-26 22:57:13 +1000

    Test Edge cases like that server is not running

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -3,30 +3,41 @@
 var server = require("./server.js");
 var http = require("http");
 
-exports.tearDown = function(done) {
-  server.stop(function() {
-    done();
-  });
-};
-
 //TODO: handle case where stop() is called before start()
-//TODO: test-drive stop() callback
-
-exports.test_serverReturnsHelloWorld = function(test) {
-  server.start(); //TODO: remove duplication?
+exports.test_serverReturnsHelloWorld = function (test) {
+  server.start(8080);
   var request = http.get("http://localhost:8080");
-  request.on("response", function(response) {
+
+  request.on("response", function (response) {
     var receivedData = false;
     response.setEncoding("utf8");
 
     test.equals(200, response.statusCode, "status code");
-    response.on("data", function(chunk) {
+    response.on("data", function (chunk) {
       receivedData = true;
       test.equals("Hello World", chunk, "response text");
     });
-    response.on("end", function() {
+
+    response.on("end", function () {
       test.ok(receivedData, "should have received response data");
-      test.done();
+      server.stop(function () {
+        test.done();
+      });
     });
   });
+};
+
+exports.test_serverRunCallbackWhenServerStopCompletes = function (test) {
+  server.start(8080); //TODO: weird
+
+  server.stop(function () {
+    test.done();
+  });
+};
+
+exports.test_stopCalledWhenServerIsNotRunningThrowException = function (test) {
+  test.throws(function () {
+    server.stop();
+  });
+  test.done();
 };
