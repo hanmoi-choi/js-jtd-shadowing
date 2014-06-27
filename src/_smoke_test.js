e286597820fa8e793a497e4ac7b25cb83337f18c
Hanmoi Choi <dchoi@192-168-1-5.tpgi.com.au>
2014-06-27 22:59:11 +1000

    procFile parsing and package.json for heroku

--- src/_smoke_test.js
+++ src/_smoke_test.js
@@ -10,6 +10,7 @@
 	var jake = require("jake");
 	var child_process = require("child_process");
 	var http = require("http");
+  var fs = require("fs");
 	var child;
 
 	exports.setUp = function(done) {
@@ -24,7 +25,7 @@
 	};
 
 	exports.test_canGetHomePage = function(test) {
-		httpGet("http://localhost:8080", function(response, receivedData) {
+		httpGet("http://localhost:5000", function(response, receivedData) {
 			var foundHomePage = receivedData.indexOf("WeeWikiPaint home page") !== -1;
 			test.ok(foundHomePage, "home page should have contained test marker");
 			test.done();
@@ -32,19 +33,35 @@
 	};
 
 	exports.test_canGet404Page = function(test) {
-		httpGet("http://localhost:8080/nonexistant.html", function(response, receivedData) {
+		httpGet("http://localhost:5000/nonexistant.html", function(response, receivedData) {
 			var foundHomePage = receivedData.indexOf("WeeWikiPaint 404 page") !== -1;
 			test.ok(foundHomePage, "404 page should have contained test marker");
 			test.done();
-		});	};
+		});
+  };
 
 	function runServer(callback) {
-		child = child_process.spawn("node", ["src/server/weewikipaint", "8080"]);
+    var web = parseProcfile();
+
+		child = child_process.spawn(web.command, web.options);
 		child.stdout.setEncoding("utf8");
 		child.stdout.on("data", function(chunk) {
 			if (chunk.trim() === "Server started") callback();
 		});
 	}
+
+  function parseProcfile() {
+    var procfile = require("procfile");
+    var file = fs.readFileSync("Procfile", "utf8");
+    var parsed = procfile.parse(file);
+    var web = parsed.web;
+    web.options = web.options.map(function(e){
+      if(e === "$PORT") return "5000";
+      else return e;
+    });
+
+    return web;
+  }
 
 	function httpGet(url, callback) {
 		var request = http.get(url);
